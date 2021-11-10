8e6f35ae2ab7ff521ccc6fc23a1ed0659bf269ee
剑器近 <zhihao.ye@qq.com>
2021-11-10 17:55:14 +0800

    deployed 2.0.6.RELEASE，优化部分代码、升级相关依赖

--- src/main/java/io/github/yezhihao/netmc/core/HandlerInterceptor.java
+++ src/main/java/io/github/yezhihao/netmc/core/HandlerInterceptor.java
@@ -9,18 +9,18 @@ import io.github.yezhihao.netmc.session.Session;
  * home https://gitee.com/yezhihao/jt808-server
  */
 public interface HandlerInterceptor<T extends Message> {
-    /** 未找到对应的Handle */
+    /** @return Response 未找到对应的Handle */
     T notSupported(T request, Session session);
 
-    /** 调用之前 */
+    /** @return boolean 调用之前 */
     boolean beforeHandle(T request, Session session);
 
-    /** 调用之后，返回值为void的 */
+    /** @return Response 调用之后，返回值为void的 */
     T successful(T request, Session session);
 
     /** 调用之后，有返回值的 */
     void afterHandle(T request, T response, Session session);
 
-    /** 调用之后抛出异常的 */
+    /** @return Response 调用之后抛出异常的 */
     T exceptional(T request, Session session, Exception e);
 }
